d395c3cd1d678f33021f91f7c11c86875ae9facf
Eoin <eoin.sheerin.2021@mumail.ie>
2026-04-17 19:22:30 +0100

    added multiple packet tests and updated thresholds

--- Vitis/src/main.c
+++ Vitis/src/main.c
@@ -1,17 +1,23 @@
+// IMPORTANT BAUD RATE IS 115200
+
 #include "sleep.h"
 #include "xil_io.h"
 #include "xparameters.h"
 #include "xuartlite_l.h"
 #include <stdint.h>
+#include <string.h>
 
-#include "ids_test_packets.h"
-#include "ids_ddos_packets.h"
-#include "ids_syn_packets.h"
-#include "ids_test_suite.h"
-#include "ids_normal_packets.h"
-#include "syn_packet_real_world.h"
+// #include "Packets/ids_test_packets.h"
+#include "Packets/icmp_packet_real_world.h"
+// #include "Packets/null_packet_real_world.h"
+// #include "Packets/syn_packet_real_world.h"
+// #include "Packets/udp_packet_real_world.h"
+// #include "Packets/xmas_packet_real_world.h"
+// #include "Packets/mixed_packets_real_world.h"
+// #include "Packets/normal_packets_real_world.h"
 
-
+#define ACTIVE_DATASET icmp_packet_real_world
+#define ACTIVE_COUNT icmp_packet_real_world_count
 
 #define GPIO_PKT_BASE XPAR_AXI_GPIO_PKT_BASEADDR
 #define GPIO_CTRL_BASE XPAR_AXI_GPIO_TRIG_BASEADDR
@@ -25,17 +31,17 @@
 #define UART_BASE XPAR_AXI_UARTLITE_0_BASEADDR
 
 #define CTRL_TVALID (1u << 0)
-#define CTRL_TLAST  (1u << 1)
+#define CTRL_TLAST (1u << 1)
 
-#define MASK_VOL_ALERT  (1u << 31)
-#define MASK_SYN_ALERT  (1u << 30)
+#define MASK_VOL_ALERT (1u << 31)
+#define MASK_SYN_ALERT (1u << 30)
 #define MASK_ICMP_ALERT (1u << 29)
-#define MASK_UDP_ALERT  (1u << 28)
+#define MASK_UDP_ALERT (1u << 28)
 #define MASK_XMAS_ALERT (1u << 27)
 #define MASK_NULL_ALERT (1u << 26)
 
-#define PKTINFO_SYN  (1u << 26)
-#define PKTINFO_UDP  (1u << 28)
+#define PKTINFO_SYN (1u << 26)
+#define PKTINFO_UDP (1u << 28)
 #define PKTINFO_ICMP (1u << 27)
 #define PKTINFO_XMAS (1u << 25)
 #define PKTINFO_NULL (1u << 24)
@@ -60,10 +66,8 @@ typedef struct {
 } target_stats_t;
 
 static target_stats_t targets[] = {
-    {0xC0A80A03, "VMware-VM1"},
-    {0xC0A80A04, "VMware-VM2"},
-    {0xC0A80A05, "Server1"},
-    {0xC0A80A06, "Server2"},
+    {0xC0A80A03, "VMware-VM1"}, {0xC0A80A04, "VMware-VM2"},
+    {0xC0A80A05, "Server1"},    {0xC0A80A06, "Server2"},
     {0xC0A80A07, "VMware-VM3"},
 };
 
@@ -272,16 +276,16 @@ static void print_attacks_detected(void) {
       uart_puts(" UDP packets)\r\n");
     }
 
-    if (dynamic_targets[i].icmp) {
+    if (dynamic_targets[i].icmp > 5) {
       uart_puts("  [!] ICMP Flood (");
       uart_put_dec(dynamic_targets[i].icmp);
       uart_puts(" ICMP packets)\r\n");
     }
 
-    if (dynamic_targets[i].xmas)
+    if (dynamic_targets[i].xmas > 3)
       uart_puts("  [!] XMAS Scan\r\n");
 
-    if (dynamic_targets[i].nulls)
+    if (dynamic_targets[i].nulls > 3)
       uart_puts("  [!] NULL Scan\r\n");
 
     if (dynamic_targets[i].total > 2) {
@@ -295,20 +299,27 @@ static void print_attacks_detected(void) {
 }
 
 int main(void) {
+    
+
+    uart_puts("\r\n=====================================\r\n");
+  uart_puts("        EE695 FPGA Intrusion Eetection \r\n");
+  uart_puts("=====================================\r\n\r\n");
+
+    
   window_summary_t windows[MAX_WINDOWS];
   uint32_t win_count = 0;
   uint32_t last_win = 0;
 
   Xil_Out32(GPIO_CTRL_BASE, 0);
-  uint16_t vol_threshold = 2;
-  uint16_t flood_threshold = 1;
+  uint16_t vol_threshold = 15;
+  uint16_t flood_threshold = 10;
 
   uint32_t threshold_val = ((uint32_t)flood_threshold << 16) | vol_threshold;
   Xil_Out32(GPIO_THR_BASE, threshold_val);
 
-  for (uint32_t i = 0; i < ids_test_packets_count; i++) {
-    send_packet(&ids_test_packets[i]);
-    usleep(20000); //changes packets per window
+  for (uint32_t i = 0; i < ACTIVE_COUNT; i++) {
+    send_packet(&ACTIVE_DATASET[i]);
+    usleep(20000); // changes packets per window
 
     uint32_t cur = Xil_In32(GPIO_WINID_BASE);
 
@@ -326,9 +337,33 @@ int main(void) {
       last_win++;
     }
 
-    uint32_t dst = Xil_In32(GPIO_DST_BASE);
-    uint32_t info = Xil_In32(GPIO_PKTINFO_BASE);
-    update_targets(dst, info);
+    usleep(50);  // or 100
+
+//     uint32_t dst = Xil_In32(GPIO_DST_BASE);
+//     uint32_t info = Xil_In32(GPIO_PKTINFO_BASE);
+// //     uart_puts("DBG dst=");
+// // uart_put_ip(dst);
+// // uart_puts(" info=0x");
+// // uart_put_dec(info);   
+//     update_targets(dst, info);
+  
+  uint32_t dst1, dst2;
+uint32_t info1, info2;
+
+do {
+    dst1  = Xil_In32(GPIO_DST_BASE);
+    info1 = Xil_In32(GPIO_PKTINFO_BASE);
+
+    dst2  = Xil_In32(GPIO_DST_BASE);
+    info2 = Xil_In32(GPIO_PKTINFO_BASE);
+
+} while (dst1 != dst2 || info1 != info2);
+
+uint32_t dst  = dst1;
+uint32_t info = info1;
+
+update_targets(dst, info);
+  
   }
 
   usleep(700000);
